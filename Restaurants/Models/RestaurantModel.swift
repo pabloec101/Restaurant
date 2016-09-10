60dedebd199e40b6caa8f5bf96cc889a6d104aa5
Juan Escobar <pabloec101@gmail.com>
2016-09-10 10:40:53 -0500

    Correcciòn navegaciòn desde la lista hasta la pantalla de detail

--- Restaurants/Models/RestaurantModel.swift
+++ Restaurants/Models/RestaurantModel.swift
@@ -12,7 +12,7 @@ class RestaurantModel: NSObject {
     
     // MARK: Mocks
     func restaurantsMock() -> [Restaurant] {
-        let restaurants:[Restaurant] = [Restaurant.init(name: "El Mexicano", address: "Cra 80", latitude: 3.23333, longitude: -4.3333, category: "Mexicana", image: "mexican-restaurant"),Restaurant.init(name: "El Desayunadero", address: "Cra 80", latitude: 3.23333, longitude: -4.3333, category: "Mexicana", image: "breakfast-restaurant"),Restaurant.init(name: "Carnicero", address: "Cra 80", latitude: 3.23333, longitude: -4.3333, category: "Mexicana", image: "parrilla-restaurant")]
+        let restaurants:[Restaurant] = [Restaurant.init(name: "El Mexicano", address: "Cra 80", latitude: 6.23333, longitude: -75.3333, category: "Mexicana", image: "mexican-restaurant"),Restaurant.init(name: "El Desayunadero", address: "Cra 80", latitude: 3.23333, longitude: -4.3333, category: "Mexicana", image: "breakfast-restaurant"),Restaurant.init(name: "Carnicero", address: "Cra 80", latitude: 3.23333, longitude: -4.3333, category: "Mexicana", image: "parrilla-restaurant")]
         
         return restaurants
     }
